3b6cbe72056f7000d726261bdad3001e6d81900e
pritanshi-gupta <guptapritanshi@gmail.com>
2026-04-12 11:24:09 +0530

    Initial commit

---
+++ BookApplication/src/main/java/com/example/BookApplication/Entity/Book.java
@@ -0,0 +1,26 @@
+package com.example.BookApplication.Entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+
+
+public class Book {
+    @Id
+    @GeneratedValue
+
+
+    private integer id;
+    private String title;
+    private String author;
+    private String genre;
+}
---
+++ BookApplication/src/main/java/com/example/BookApplication/Service/BookService.java
@@ -0,0 +1,8 @@
+package com.example.BookApplication.Service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+
+public class BookService {
+}
